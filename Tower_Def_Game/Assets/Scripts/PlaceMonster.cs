bdcb71307b0029fbf7dc1c29419dce9234384c8c
Carlson-poly <carlsonzhong@gmail.com>
2023-06-23 13:02:41 +1200

    assessment tasks

--- Tower_Def_Game/Assets/Scripts/PlaceMonster.cs
+++ Tower_Def_Game/Assets/Scripts/PlaceMonster.cs
@@ -6,11 +6,12 @@ public class PlaceMonster : MonoBehaviour
 {
     public GameObject monsterPrefab;
     private GameObject monster;
+    private GameManagerBehaviour gameManager;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManagerBehaviour>();
     }
 
     // Update is called once per frame
@@ -20,8 +21,10 @@ public class PlaceMonster : MonoBehaviour
     }
     private bool CanPlaceMonster()
     {
-        return monster == null;
+        int cost = monsterPrefab.GetComponent<MonsterData>().levels[0].cost;
+        return monster == null && gameManager.Gold >= cost;
     }
+
     void OnMouseUp()
     {
         if (CanPlaceMonster())
@@ -30,15 +33,14 @@ public class PlaceMonster : MonoBehaviour
 
             AudioSource audioSource = gameObject.GetComponent<AudioSource>();
             audioSource.PlayOneShot(audioSource.clip);
-
-            // TODO: Deduct gold
+            gameManager.Gold -= monster.GetComponent<MonsterData>().CurrentLevel.cost;
         }
         else if (CanUpgradeMonster())
         {
             monster.GetComponent<MonsterData>().IncreaseLevel();
             AudioSource audioSource = gameObject.GetComponent<AudioSource>();
             audioSource.PlayOneShot(audioSource.clip);
-            // TODO: Deduct gold
+           gameManager.Gold -= monster.GetComponent<MonsterData>().CurrentLevel.cost;
         }
     }
 
@@ -50,7 +52,7 @@ public class PlaceMonster : MonoBehaviour
         MonsterLevel nextLevel = monsterData.GetNextLevel();
         if (nextLevel != null)
         {
-        return true;
+        return gameManager.Gold >= nextLevel.cost;
         }
     }
     return false;
